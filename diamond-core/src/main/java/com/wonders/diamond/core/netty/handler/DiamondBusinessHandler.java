10c32ac1da887dd6b98cf601e4aa946e041d5821
nick <qiujun4417@hotmail.com>
2017-06-23 17:50:06 +0800

    增加回写

--- diamond-core/src/main/java/com/wonders/diamond/core/netty/handler/DiamondBusinessHandler.java
+++ diamond-core/src/main/java/com/wonders/diamond/core/netty/handler/DiamondBusinessHandler.java
@@ -1,8 +1,21 @@
 package com.wonders.diamond.core.netty.handler;
 
+import com.wonders.diamond.core.exceptions.BaseException;
 import com.wonders.diamond.core.netty.DiamondRequest;
+import com.wonders.diamond.core.netty.DiamondResponse;
+import com.wonders.diamond.core.utils.JsonConvertor;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.CharsetUtil;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 /**
  * Created by ningyang on 2017/1/14.
@@ -23,11 +36,24 @@ public class DiamondBusinessHandler extends SimpleChannelInboundHandler<DiamondR
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DiamondRequest diamondRequest) throws Exception {
-        diamondRequestHandler.handleDiamondRequest(diamondRequest);
-
+        DiamondResponse diamondResponse = diamondRequestHandler.handleDiamondRequest(diamondRequest);
+        printMessage(ctx, diamondResponse);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    }
+
+    private void printMessage(ChannelHandlerContext ctx, DiamondResponse response){
+        String str = JsonConvertor.toJson(response);
+        ByteBuf content = Unpooled.copiedBuffer(str, CharsetUtil.UTF_8);
+        FullHttpResponse fullHttpResponse = new DefaultFullHttpResponse(
+                HTTP_1_1,
+                HttpResponseStatus.OK,
+                content
+        );
+        fullHttpResponse.headers().add(CONTENT_TYPE, "application/json; charset=UTF-8");
+        // Close the connection as soon as the error message is sent.
+        ctx.writeAndFlush(fullHttpResponse).addListener(ChannelFutureListener.CLOSE);
     }
 }
